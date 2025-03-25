d635ab363a85a7b9e24ec3bd261ef86a9fb2e1bc
suyeon06 <suyeonchae0919@gm.gist.ac.kr>
2025-03-25 23:51:14 +0900

    add ChitChat

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,13 @@ function App() {
   return (
     <>
       <div>
+      <div class="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
+  <img class="size-12 shrink-0" src="/img/logo.svg" alt="ChitChat Logo"/>
+  <div>
+    <div class="text-xl font-medium text-black dark:text-white">ChitChat</div>
+    <p class="text-gray-500 dark:text-gray-400">You have a new message!</p>
+  </div>
+</div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,6 +34,8 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
+
+
       </p>
     </>
   )
